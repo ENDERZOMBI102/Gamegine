b29437f1cd75d398acc7d315b4b836bdbd83f6a6
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-03-01 23:24:29 +0100

    initial code
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

---
+++ Sandbox/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Gamengine;
+
+namespace Sandbox {
+	static class Program {
+		static void Main(string[] args) {
+			Engine engine = new Engine();
+			engine.Run();
+		}
+	}
+}
